da0db418a0b51cef470f0b65f9fa97d9ae4e7817
Avinash9997 <126712293+Avinash9997@users.noreply.github.com>
2023-05-31 22:39:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import "../styles/App.css";
 
 var colors = [
@@ -17,14 +17,50 @@ var colors = [
 ];
 
 const App = () => {
+const [quote, setQuote] = useState(null);
+const [backgroundColor, setBackgroundColor] = useState("#333");
 
-    return (
-      <div id="main">
-        <div id="wrapper">
-          
-        </div>
-      </div>
-    );
+useEffect(() => {
+    fetchQuote();
+}, []);
+
+const fetchQuote = async () => {
+    try {
+        const response = await fetch("https://api.quotable.io/random");
+        const data = await response.json();
+        setQuote(data);
+        changeBackgroundColor();
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-export default App;
+const changeBackgroundColor = () => {
+    const colorIndex = Math.floor(Math.random() * colors.length);
+    const color = colors[colorIndex];
+    setBackgroundColor(color);
+};
+
+return (
+    <div id="main" style={{ backgroundColor }}>
+        <div id="wrapper">
+             {quote && (
+             <div id="quote-box">
+                 <div className="quote-text">{quote.content}</div>
+                 <div className="quote-author">- {quote.author}</div>
+                 <div className="buttons">
+                     <button
+                             id="new-quote"
+                             className="button"
+                             onClick={fetchQuote}
+                           >
+                      New Quote
+                    </button>
+                </div>
+           </div>
+           )}
+        </div>
+    </div>
+  );
+};
+export default App;
